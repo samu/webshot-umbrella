5f14272f636c38ee5b47b29bc18fe7d6dae2975c
Samuel Mueller <mueller.samu@gmail.com>
2016-07-14 06:36:53 +0200

    rename test description

--- apps/webapp/test/controllers/page_controller_test.exs
+++ apps/webapp/test/controllers/page_controller_test.exs
@@ -1,7 +1,7 @@
 defmodule Webapp.UserControllerTest do
   use Webapp.ConnCase
 
-  test "produces a background job for taking a snapshot", %{conn: conn} do
+  test "produces a message :take_snapshot", %{conn: conn} do
     Phoenix.PubSub.subscribe Webapp.Queue, "webshot:take"
     conn = get conn, page_path(conn, :webshot)
     assert response(conn, 200) =~ "yep!"
