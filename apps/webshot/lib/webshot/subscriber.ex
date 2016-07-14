916c6e7ec065653367c8d0fcba7afc07d2b7999a
Samuel Mueller <mueller.samu@gmail.com>
2016-07-14 23:06:28 +0200

    only subscribe if queue exists

--- apps/webshot/lib/webshot/subscriber.ex
+++ apps/webshot/lib/webshot/subscriber.ex
@@ -1,6 +1,9 @@
 defmodule Webshot.Subscriber do
   def start_link do
-    Phoenix.PubSub.subscribe Webapp.Queue, "webshot:take"
+    if Process.whereis(Webapp.Queue) do
+      Phoenix.PubSub.subscribe Webapp.Queue, "webshot:take"
+    end
+
     pid = spawn_link(__MODULE__, :subscribe, [])
     {:ok, pid}
   end
