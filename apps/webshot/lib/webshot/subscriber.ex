8578cfc4fa8f9ff09b0361961a058eed768c6491
Samuel Mueller <mueller.samu@gmail.com>
2016-07-15 09:05:32 +0200

    :angry:

--- apps/webshot/lib/webshot/subscriber.ex
+++ apps/webshot/lib/webshot/subscriber.ex
@@ -1,21 +1,46 @@
 defmodule Webshot.Subscriber do
-  def start_link do
+  use GenServer
+
+  def start_link(work \\ fn -> default_work() end) do
+    me = self
     if Process.whereis(Webapp.Queue) do
-      Phoenix.PubSub.subscribe Webapp.Queue, "webshot:take"
+      IO.puts "----------- subscribing!!"
+      Phoenix.PubSub.subscribe me, Webapp.Queue, "webshot:take"
     end
 
-    pid = spawn_link(__MODULE__, :subscribe, [])
-    {:ok, pid}
+    GenServer.start_link(__MODULE__, {work}, name: __MODULE__)
   end
 
-  def subscribe do
-    receive do
-      message -> do_work()
-    end
-    subscribe
+  def handle_info(message, state) do
+    IO.puts "RECEIVED!!!!!!!!!!!!"
+    {:noreply, state}
   end
 
-  defp do_work do
+  def default_work do
     Webshot.Server.take_snapshot(self, "url")
   end
+
+  # def start_link(work \\ fn -> default_work() end) do
+  #   me = self
+  #   if Process.whereis(Webapp.Queue) do
+  #     IO.puts "----------- subscribing!!"
+  #     Phoenix.PubSub.subscribe me, Webapp.Queue, "webshot:take"
+  #   end
+  #
+  #   pid = spawn_link(__MODULE__, :subscribe, [work])
+  #   {:ok, pid}
+  # end
+  #
+  # def default_work do
+  #   Webshot.Server.take_snapshot(self, "url")
+  # end
+  #
+  # def subscribe(work) do
+  #   receive do
+  #     message ->
+  #       IO.puts "----------- RECEIVED!!!"
+  #       work.()
+  #   end
+  #   subscribe(work)
+  # end
 end
--- apps/webshot/test/subscriber_test.exs
+++ apps/webshot/test/subscriber_test.exs
@@ -1,17 +1,31 @@
 defmodule SubscriberTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case
 
   setup do
+    me = self
+    work = fn -> send me, {:message_received} end
+
     Supervisor.start_child(Webshot.Supervisor,
       Supervisor.Spec.supervisor(Phoenix.PubSub.PG2,
       [Webapp.Queue, [pool_size: 1]]))
-    Supervisor.start_child(Webshot.Supervisor, Webshot.subscriber_worker)
+    # sup = Supervisor.Spec.supervisor(Phoenix.PubSub.PG2,
+    #   [Webapp.Queue, [pool_size: 1]])
+
+    # opts = [strategy: :one_for_one, name: Webshot.Queue.Supervisor]
+    # Supervisor.start_link([sup], opts)
+
+    Supervisor.start_child(Webshot.Supervisor,
+      Supervisor.Spec.worker(Webshot.Subscriber, [work]))
 
     :ok
   end
 
   describe "when subscribed to a channel" do
     test "it receives related messages" do
+      Phoenix.PubSub.broadcast Webapp.Queue, "webshot:take",
+        {:take_webshot, "test_url"}
+
+      # assert_receive {:message_received}
     end
   end
 end
